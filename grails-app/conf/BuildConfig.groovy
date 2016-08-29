49e590853147d2398dd43b6512e8b1251123d5fe
temi <temivarg@gmail.com>
2016-08-30 08:07:28 +1000

    added group id and war file dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,8 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.7
 grails.project.source.level = 1.7
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.groupId = "au.org.ala"
+grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.server.port.http = 8081
 
