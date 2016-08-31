17318c7865eb4ad58805473b8baefebb32d4b002
temi <temivarg@gmail.com>
2016-08-31 18:08:02 +1000

    AtlasOfLivingAustralia/spatial-hub#26 fixes google places z-index

--- web-app/js/portal/directive/googlePlaces.js
+++ web-app/js/portal/directive/googlePlaces.js
@@ -6,7 +6,7 @@
             replace: true,
             // transclude:true,
             scope: {location: '=', name: '='},
-            template: '<input style="z-index:2000;width:300px" id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
+            template: '<input style="width:300px" id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
             link: function ($scope, elm, attrs) {
                 var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
                 google.maps.event.addListener(autocomplete, 'place_changed', function () {
