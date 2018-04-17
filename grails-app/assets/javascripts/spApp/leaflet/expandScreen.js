8c5cab2ed71b25b512f161e0303deca959e8e0eb
Rita Chen <Rita.Chen@csiro.au>
2018-04-17 15:49:26 +1000

    AtlasOfLivingAustralia/spatial-hub#85 Change expand map control titles once clicked.

--- grails-app/assets/javascripts/spApp/leaflet/expandScreen.js
+++ grails-app/assets/javascripts/spApp/leaflet/expandScreen.js
@@ -2,7 +2,9 @@ L.Control.Expand = L.Control.extend({
     options: {
         position: 'topleft',
         titleLeft: 'Expand Map Left',
+        titleRight: 'Expand Map Right',
         titleUp: 'Expand Map Up',
+        titleDown: 'Expand Map Down',
         toggleExpandLeft: null,
         toggleExpandUp: null
     },
@@ -21,27 +23,29 @@ L.Control.Expand = L.Control.extend({
 
         map.expandControl = this;
 
-        var up = L.DomUtil.create('a', 'icon-expand-up', container);
-        up.href = '#';
-        up.title = this.options.titleUp;
-        L.DomEvent.addListener(up, 'click', L.DomEvent.stopPropagation)
-            .addListener(up, 'click', L.DomEvent.preventDefault)
-            .addListener(up, 'click', this._toggleUp, map);
+        this.up = L.DomUtil.create('a', 'icon-expand-up', container);
+        this.up.href = '#';
+        this.up.title = this.options.titleUp;
+        L.DomEvent.addListener(this.up, 'click', L.DomEvent.stopPropagation)
+            .addListener(this.up, 'click', L.DomEvent.preventDefault)
+            .addListener(this.up, 'click', this._toggleUp, map);
 
-        var left = L.DomUtil.create('a', 'icon-expand-left', container);
-        left.href = '#';
-        left.title = this.options.titleLeft;
-        L.DomEvent.addListener(left, 'click', L.DomEvent.stopPropagation)
-            .addListener(left, 'click', L.DomEvent.preventDefault)
-            .addListener(left, 'click', this._toggleLeft, map);
+        this.left = L.DomUtil.create('a', 'icon-expand-left', container);
+        this.left.href = '#';
+        this.left.title = this.options.titleLeft;
+        L.DomEvent.addListener(this.left, 'click', L.DomEvent.stopPropagation)
+            .addListener(this.left, 'click', L.DomEvent.preventDefault)
+            .addListener(this.left, 'click', this._toggleLeft, map);
 
         return container;
     },
 
     _toggleUp: function() {
         if (this.expandControl._up_state) {
+            this.expandControl.up.title = this.expandControl.options.titleUp;
             $(".icon-expand-up").removeClass("icon-expand-down")
         } else {
+            this.expandControl.up.title = this.expandControl.options.titleDown;
             $(".icon-expand-up").addClass("icon-expand-down")
         }
         this.expandControl.options.toggleExpandUp(this);
@@ -50,8 +54,10 @@ L.Control.Expand = L.Control.extend({
 
     _toggleLeft: function() {
         if (this.expandControl._left_state) {
+            this.expandControl.left.title = this.expandControl.options.titleLeft;
             $(".icon-expand-left").removeClass("icon-expand-right")
         } else {
+            this.expandControl.left.title = this.expandControl.options.titleRight;
             $(".icon-expand-left").addClass("icon-expand-right")
         }
         this.expandControl.options.toggleExpandLeft(this);
