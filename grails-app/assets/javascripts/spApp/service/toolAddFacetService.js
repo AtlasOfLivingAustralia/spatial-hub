8803db7f5ac9991e91e78bf0fe74a42d3529f699
Qifeng <qifeng.bai@csiro.au>
2018-07-10 15:01:50 +1000

    names for adding facets

--- grails-app/assets/javascripts/spApp/service/toolAddFacetService.js
+++ grails-app/assets/javascripts/spApp/service/toolAddFacetService.js
@@ -51,7 +51,20 @@
                     else if (speciesOptions.spatiallyValid) q.push('geospatial_kosher:true');
                     else if (speciesOptions.spatiallySuspect) q.push('geospatial_kosher:false');
                     var newName = $i18n("Facet");
-                    if (area.name !== undefined) newName += ' (' + area.name + ')';
+                    //if (area.name !== undefined) newName += ' (' + area.name + ')';
+                    //Guess name from factet Genuse:"Cractus" OR Genuse:"xxxxxx"
+                    var classes = facet.split("OR");
+                    var classesname = []
+                    for (var i in classes){
+                        classesname.push(classes[0].split(":")[1].replace(/['"]+/g, ''))
+                    }
+                    newName += ' ('+classesname.join('/')+')'
+                    // if (classes.length>1){
+                    //     newName = facet;
+                    // }else{
+                    //     newName += ' ('+classes[0].split(":")[1].replace(/['"]+/g, '')+')'
+                    // }
+
                     return BiocacheService.newLayer({
                         q: q,
                         bs: $SH.biocacheServiceUrl,
