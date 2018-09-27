6ae5273c36f28b5bed58eba83fd150044c007eef
adam-collins <adam.collins832@gmail.com>
2018-09-27 10:17:57 +1000

    Support external layout file and assets.

--- grails-app/init/au/org/ala/spatial/portal/BootStrap.groovy
+++ grails-app/init/au/org/ala/spatial/portal/BootStrap.groovy
@@ -1,20 +1,23 @@
 package au.org.ala.spatial.portal
 
+import asset.pipeline.AssetPipelineConfigHolder
+import asset.pipeline.fs.FileSystemAssetResolver
 import au.org.ala.cas.util.AuthenticationUtils
 import au.org.ala.web.AuthService
 import grails.converters.JSON
-import org.apache.naming.ContextAccessController
+import org.apache.commons.io.FileUtils
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
 import org.springframework.web.context.request.RequestContextHolder
 
-import javax.naming.InitialContext
-import javax.naming.NamingException
 import javax.servlet.http.HttpServletRequest
-import java.lang.reflect.Field
 
 class BootStrap {
 
     def portalService
     def grailsApplication
+    def groovyPageLocator
 
     def init = { servletContext ->
 
@@ -29,7 +32,73 @@ class BootStrap {
             grailsApplication.config.startup.baselayers = JSON.parse(grailsApplication.config.startup.baselayers)
         }
 
+        // support for external layouts
+        //
+        // e.g. config "skin.layout=myLayout" will use the layout file /data/spatial-hub/views/layouts/myLayout.gsp
+        //
+        // support external files with the asset tag in the layout gsp
+        // e.g.
+        //   files
+        //     /data/spatial-hub/assets/css/externalCss.css
+        //     /data/spatial-hub/assets/js/externalJs.js
+        //     /data/spatial-hub/assets/img/externalImage.png
+        //   asset tags
+        //     <asset:stylesheet src="css/externalCss.css" />
+        //     <asset:javascript src="js/externalJs.js" />
+        //     <asset:image src="img/externalImg.png" />
+        //
+        // Support the use of the default "skin.layout=generic" with a local "headerAndFooter.baseURL" by the creating files
+        //   /data/spatial-hub/assets/css/bootstrap.min.css
+        //   /data/spatial-hub/assets/css/ala-styles.css
+        addExternalViews()
+        addExternalAssets(servletContext)
+
         portalService.updateListQueries()
+    }
+
+    def addExternalViews = {
+        groovyPageLocator.addResourceLoader(new ResourceLoader() {
+
+            @Override
+            Resource getResource(String s) {
+                File resource = new File('/data/spatial-hub/views' + s)
+                if (resource.exists()) {
+                    return new FileSystemResource(resource)
+                }
+                return null
+            }
+
+            @Override
+            ClassLoader getClassLoader() {
+                return null
+            }
+        })
+    }
+
+    def addExternalAssets = { servletContext ->
+        try {
+            File src = new File('/data/spatial-hub/assets')
+
+            if (src.exists() && src.isDirectory()) {
+                // asset-pipeline resolves files differently depending on the presence of a manifest
+                if (AssetPipelineConfigHolder.manifest) {
+                    // copy external asset files into expanded war assets directory so they can be discovered
+                    File dst = new File(servletContext.getRealPath("/assets"))
+
+                    if (dst.exists() && dst.isDirectory()) {
+                        FileUtils.copyDirectory(src, dst)
+                    } else {
+                        log.error("External assets are unavailable. Expanded WAR asset directory '${dst.path}' does not exist.")
+                    }
+                } else {
+                    // a new Resolver is required to find the external assets at runtime
+                    def resolver = new FileSystemAssetResolver("external-assets", '/data/spatial-hub/assets', false)
+                    AssetPipelineConfigHolder.registerResolver(resolver)
+                }
+            }
+        } catch (err) {
+            log.error("External assets are not available.", err)
+        }
     }
 
     def destroy = {
