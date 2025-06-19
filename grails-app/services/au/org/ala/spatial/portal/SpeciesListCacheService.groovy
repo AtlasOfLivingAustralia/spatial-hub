c8578f80564615e062f851c851460b6b05795017
kshaleninova <katya@gleveconsult.com>
2025-06-19 09:12:03 +0200

    #617 - use GrailsCacheManager instead of @Cacheable configuration as the latter does not handle well the errors

--- grails-app/services/au/org/ala/spatial/portal/SpeciesListCacheService.groovy
+++ grails-app/services/au/org/ala/spatial/portal/SpeciesListCacheService.groovy
@@ -6,44 +6,53 @@ import grails.plugin.cache.Cacheable
 class SpeciesListCacheService {
     def grailsApplication
     def hubWebService
+    def grailsCacheManager
 
-    @Cacheable("threatenedQCache")
     def getThreatenedQ() {
-        log.info("Refreshing the threatened species lists query in threatenedQCache")
-        if (grailsApplication.config.lists.url != '') {
-            try {
-                def threatenedQ = ''
-                def threatenedUrl = "${grailsApplication.config.lists.url}${grailsApplication.config.lists.threatenedSpeciesUrl}"
-                def threatened = JSON.parse(hubWebService.getUrl(threatenedUrl, null, false)) as Map
-                def threatenedJoined = threatened.lists*.dataResourceUid.join(' OR ')
-                if (threatenedJoined) {
-                    threatenedQ = "species_list_uid:(${threatenedJoined})"
+        log.info("Refreshing the threatened species lists query for threatenedQCache")
+        def cachedThreatenedQ = grailsCacheManager.getCache("threatenedQCache").get("threatenedQ")
+        if (!cachedThreatenedQ) {
+            def threatenedQ = ''
+            if (grailsApplication.config.lists.url != '') {
+                try {
+                    def threatenedUrl = "${grailsApplication.config.lists.url}${grailsApplication.config.lists.threatenedSpeciesUrl}"
+                    def threatened = JSON.parse(hubWebService.getUrl(threatenedUrl, null, false)) as Map
+                    def threatenedJoined = threatened.lists*.dataResourceUid.join(' OR ')
+                    if (threatenedJoined) {
+                        threatenedQ = "species_list_uid:(${threatenedJoined})"
+                    }
+                } catch (err) {
+                    log.error("Failed to fetch threatened species lists query for threatenedQCache. Next cache refresh attempt will occur in 15 minutes. You can invalidate caches explicitly with resetCaches method. ", err)
                 }
-                return threatenedQ
-            } catch (err) {
-                log.error("Failed to fetch threatened species lists query in threatenedQCache. Caching empty value. Next cache refresh attempt will occur in 15 minutes", err)
-                return ''
+            } else {
+                log.warn("No species lists URL configured in application. Threatened species lists query for threatenedQCache is set to empty value.")
             }
+            grailsCacheManager.getCache("threatenedQCache").put("threatenedQ", threatenedQ)
         }
     }
 
-    @Cacheable("invasiveQCache")
     def getInvasiveQ() {
-        log.info("Refreshing the invasive species lists query in invasiveQCache")
-        if (grailsApplication.config.lists.url != '') {
-            try {
-                def invasiveQ = ''
-                def invasiveUrl = "${grailsApplication.config.lists.url}${grailsApplication.config.lists.invasiveSpeciesUrl}"
-                def invasive = JSON.parse(hubWebService.getUrl(invasiveUrl, null, false)) as Map
-                def invasiveJoined = invasive.lists*.dataResourceUid.join(' OR ')
-                if (invasiveJoined) {
-                    invasiveQ = "species_list_uid:(${invasiveJoined})"
+        log.info("Refreshing the invasive species lists query for invasiveQCache")
+        def cachedInvasiveQ = grailsCacheManager.getCache("invasiveQCache").get("invasiveQ")
+        if (!cachedInvasiveQ) {
+            def invasiveQ = ''
+            if (grailsApplication.config.lists.url != '') {
+                try {
+                    def invasiveUrl = "${grailsApplication.config.lists.url}${grailsApplication.config.lists.invasiveSpeciesUrl}"
+                    def invasive = JSON.parse(hubWebService.getUrl(invasiveUrl, null, false)) as Map
+                    def invasiveJoined = invasive.lists*.dataResourceUid.join(' OR ')
+                    if (invasiveJoined) {
+                        invasiveQ = "species_list_uid:(${invasiveJoined})"
+                    }
+                    return invasiveQ
+                } catch (err) {
+                    log.error("Failed to fetch invasive species lists query for invasiveQCache. Next cache refresh attempt will occur in 15 minutes. You can invalidate caches explicitly with resetCaches method.", err)
+                    return ''
                 }
-                return invasiveQ
-            } catch (err) {
-                log.error("Failed to fetch species lists query in invasiveQCache. Caching empty value. Next cache refresh attempt will occur in 15 minutes", err)
-                return ''
+            } else {
+                log.warn("No species lists URL configured in application. Invasive species lists query for invasiveQCache is set to empty value.")
             }
+            grailsCacheManager.getCache("invasiveQCache").put("invasiveQ", invasiveQ)
         }
     }
 }
