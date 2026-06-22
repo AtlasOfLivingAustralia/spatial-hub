b4f13fbd69248e3b590cdf22e88da3e4f646f246
Adam Collins <adam.collins@csiro.au>
2026-06-22 13:40:35 +1000

    #527 fix private species lists

--- grails-app/assets/javascripts/spApp/directive/listsList.js
+++ grails-app/assets/javascripts/spApp/directive/listsList.js
@@ -23,7 +23,7 @@
                         scope.pageSize = 20;
                         scope.page = 1;
                         scope.total = '';
-                        scope.sortType = '';
+                        scope.sortType = 'lastUpdated';
                         scope.sortReverse = false;
                         scope.lastSearch = scope.searchLists + scope.sortType + scope.sortReverse;
                         scope.isLoading = true;
@@ -49,6 +49,7 @@
                                     lastUpdated: data.lastUpdated,
                                     itemCount: data.itemCount,
                                     fullName: data.fullName,
+                                    isAuthoritative: data.isAuthoritative,
                                     selected: false
                                 });
                             }
@@ -59,7 +60,7 @@
                             if (data.length) {
                                 for (var i = 0; i < data.length; i++) {
                                     scope.items.push({
-                                        dataResourceUid: data[i].dataResourceUid,
+                                        dataResourceUid: data[i].dataResourceUid || data[i].id,
                                         listName: data[i].listName,
                                         lastUpdated: data[i].lastUpdated,
                                         itemCount: data[i].itemCount,
@@ -70,11 +71,12 @@
                                 }
                             } else if (data.dataResourceUid) {
                                 scope.items.push({
-                                    dataResourceUid: data.dataResourceUid,
+                                    dataResourceUid: data.dataResourceUid || data.id,
                                     listName: data.listName,
                                     lastUpdated: data.lastUpdated,
                                     itemCount: data.itemCount,
                                     fullName: data.fullName,
+                                    isAuthoritative: data.isAuthoritative,
                                     selected: false
                                 });
                             }
